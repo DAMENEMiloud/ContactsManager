dba6441816f4e6f8cdc593b80ac264ae9c95960b
Miloud DAMENE <miloud@Milouds-MacBook-Pro.local>
2020-01-25 15:11:32 +0100

    Updates

---
+++ precache-manifest.3b531cca02bf75d59ea387778a29d0ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa44efc54519eb4b79ed15b58de5255",
+    "url": "/ContactsManager/index.html"
+  },
+  {
+    "revision": "f1ca3e6d9e10ff08c2be",
+    "url": "/ContactsManager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "e39f18a0b75926ff3486",
+    "url": "/ContactsManager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "f1ca3e6d9e10ff08c2be",
+    "url": "/ContactsManager/static/js/2.510ea848.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/ContactsManager/static/js/2.510ea848.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e39f18a0b75926ff3486",
+    "url": "/ContactsManager/static/js/main.3800064f.chunk.js"
+  },
+  {
+    "revision": "40803b0548cce2e96611",
+    "url": "/ContactsManager/static/js/runtime-main.e7d705be.js"
+  }
+]);
